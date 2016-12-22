f92ea7b7379cd60a51237db9956f10e9c1259154
Gene Ryman <gene.ryman@gmail.com>
2016-12-22 10:44:50 -0500

    Fix helper.googleFilterWithKeyword()
    Fixed function so that it simply hides unused markers, rather than removing them completely.
    
    Instead of removing and replacing markers

--- dist/js/helper-old.js
+++ dist/js/helper-old.js
@@ -319,8 +319,7 @@ var helper = {
     // Hides markers
     hideMarkers: function(markers) {
         for (var i = 0; i < markers.length; i++) {
-            //markers[i].setMap(null);
-            markers[i].setVisible(false);
+            markers[i].setMap(null);
         }
     },
     // Stops marker from bouncing
@@ -359,13 +358,8 @@ var helper = {
             // Create an onclick event to open an infowindow at each marker.
             marker.addListener('click', function() {
                 self.populateInfoWindow(this, self.getInfowindow());
+                self.displayReviews();
                 self.toggleBounce(this);
-                var id = this.id;
-                model.venuesList.forEach(function(location){
-                    if (location.id === id){
-                        vm.displayReviews(location);
-                    }
-                })
             });
             marker.addListener('mouseover', function() {
                 this.setIcon(self.getHighlightedIcon());
@@ -379,7 +373,7 @@ var helper = {
         });
         model.map.fitBounds(bounds);
         google.maps.event.addDomListener(window, 'resize', function(){
-            model.map.fitBounds(bounds);
+            map.fitBounds(bounds);
         })
         //console.log(model.markers);
     },
@@ -511,37 +505,18 @@ var helper = {
         model.venuesList = [];
         // Loop through model.locations looking for matching keywords
         var i = 0;
-        var increment = false
-        // HIDE ALL MARKERS
-        this.hideMarkers(model.markers);
-
         model.locations.forEach(function(locationItem){
             // If the keyword matches, add it the venue to the list
             if (locationItem.keywords.includes(vm.keywordValue())){
                 helper.addToVenuesList(locationItem, i);
                 // MOVE THE i++ OUTSIDE OF THE IF STATEMENT
-                model.markers.forEach(function(marker){
-                    console.log(marker.id);
-                    console.log(i);
-                    if (marker.id === i){
-                        console.log(marker)
-                        marker.setVisible(true);
-                    }
-                });
+                i++;
             }
-            i++;
-            //LOOP THROUGH MARKERS, FIND THE ONE THAT MATCHES ID, SET IT TO VISIBLE
-
         });
-
-
-
         // Set value of vm.venuesListVisible to model.venuesList
         vm.venuesListVisible(model.venuesList);
         // Set filtered markers
-        //this.setMarkers();
-        //counting loop
-        //if 
+        this.setMarkers();
 
     }
     /* Searches google maps for keyword(s), filters through model.locations, and displays results with markers and venuesList
--- dist/js/viewmodel-old.js
+++ dist/js/viewmodel-old.js
@@ -5,8 +5,7 @@ var mapView = {
         helper.setMarkers();
         helper.setInfowindow();
         // Sets map on DOM
-       // document.getElementById('map').append('<a id="menu" class="header__menu"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"></svg></a>');
-        //document.getElementById('map').append('');
+        document.getElementById('map').append('<a id="menu" class="header__menu"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"></svg></a>');
     },
     handleError: function() {
         window.alert("Map could not be loaded.");
@@ -47,7 +46,7 @@ ViewModel = function() {
         model.firstReset = false;
         self.venuesListVisible(model.venuesList);
     };
-    this.displayReviews = function(location) {
+    this.displayReviews = function() {
         var that = this;
 
         //clears all reviews from all Location objects
@@ -59,24 +58,24 @@ ViewModel = function() {
         });
 
         // Set "reviewsExist" to true for this item, so that the "Reviews - Powered by Foursquare" subheader appears
-        location.reviewsExist(true);
+        this.reviewsExist(true);
 
         // Filter through markers for an ID that matches the ID of the list item and highlights that marker and creates an infowindow
         model.markers.forEach(function(marker) {
-            if (marker.id == location.id) {
+            if (marker.id == that.id) {
                 helper.affectMarker(marker);
             }
         });
         // Create a timeout request that creates a window alert and displays "No reviews to display"
-        /*var foursquareRequestTimeout = setTimeout(function() {
+        var foursquareRequestTimeout = setTimeout(function() {
             window.alert("Unable to retrieve Foursquare resources");
             that.reviews.push({
                 review: "No reviews to display!"
             });
-        }, 8000);*/
+        }, 8000);
         
         // Prepares URL for AJAX request
-        foursquareUrl = 'https://api.foursquare.com/v2/venues/' + location.VENUE_ID + '?client_id=' + model.client_id + '&client_secret=' + model.client_secret + '&v=20161129';
+        foursquareUrl = 'https://api.foursquare.com/v2/venues/' + this.VENUE_ID + '?client_id=' + model.client_id + '&client_secret=' + model.client_secret + '&v=20161129';
         // AJAX request - calls upon Foursquare API for this location and returns an array of reviews
         $.ajax({
             url: foursquareUrl,
@@ -89,19 +88,13 @@ ViewModel = function() {
                         if (i == 4) {
                             break;
                         }
-                        location.reviews.push({
+                        that.reviews.push({
                             review: '\"' + venueReviewsArray[i].text + '\"'
                         });
                     }
                 }
                 //if successful, ignore the timeout request
-                //clearTimeout(foursquareRequestTimeout);
-            },
-            error: function(err){
-                window.alert("Unable to retrieve Foursquare resources");
-                location.reviews.push({
-                  review: "No reviews to display!"
-                });
+                clearTimeout(foursquareRequestTimeout);
             }
         });
     };
